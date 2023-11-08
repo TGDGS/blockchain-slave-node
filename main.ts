b7fc20dcd379e3cad374cc4cb618789a8d5e68a8
TGDGS <111734730+TGDGS@users.noreply.github.com>
2023-11-08 11:24:51 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,18 @@
-basic.forever(function () {
-	
+enum RadioMessage {
+    message1 = 49434
+}
+radio.onReceivedValue(function (name, value) {
+    if (name == "Block_Reward") {
+        Block_Reward = value
+    } else if (name == "Block_No") {
+        Block_No = value
+    } else if (name == "Total_Blocks") {
+        Total_Blocks = value
+    }
 })
+let Total_Blocks = 0
+let Block_No = 0
+let Block_Reward = 0
+radio.setGroup(90)
+radio.setTransmitPower(7)
+radio.setFrequencyBand(5)
