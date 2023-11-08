3038d1863841f4d8dae5be0655bc5ec263d7b724
TGDGS <111734730+TGDGS@users.noreply.github.com>
2023-11-08 13:03:29 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,20 @@
 enum RadioMessage {
     message1 = 49434
 }
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Mining Starting")
+    radio.sendValue("Mining_Started", 1)
+})
 radio.onReceivedValue(function (name, value) {
     if (name == "Block_Reward") {
         Block_Reward = value
+        serial.writeNumber(value)
     } else if (name == "Block_No") {
         Block_No = value
+        serial.writeNumber(value)
     } else if (name == "Total_Blocks") {
         Total_Blocks = value
+        serial.writeNumber(value)
     }
 })
 let Total_Blocks = 0
@@ -16,3 +23,4 @@ let Block_Reward = 0
 radio.setGroup(90)
 radio.setTransmitPower(7)
 radio.setFrequencyBand(5)
+serial.redirectToUSB()
